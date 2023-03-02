041114d6c21301ab9f8d6516403bd1b0e747519a
Natalia Burduk <nataliaburduk@gmail.com>
2023-03-02 11:16:44 +0100

    Add validation for charId and onSelectedChar props

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { PropTypes } from 'prop-types';
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../Spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -96,6 +97,10 @@ class CharList extends Component {
         </div>
     )
    }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired
 }
 
 export default CharList;
