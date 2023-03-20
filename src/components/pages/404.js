b8b589eb34a3b3d4d2698322765e66a3dd669ad3
Natalia Burduk <nataliaburduk@gmail.com>
2023-03-20 15:48:27 +0100

    Add SEO-optimization with React Helmet

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,9 +1,17 @@
-import ErrorMessage from "../errorMessage/ErrorMessage"
+import { Helmet } from "react-helmet"
 import { Link } from "react-router-dom"
+import ErrorMessage from "../errorMessage/ErrorMessage"
 
 const Page404 = () => {
   return (
     <div>
+      <Helmet>
+        <meta
+            name="description"
+            content="Error message page"
+          />
+          <title>Page does not exist</title>
+      </Helmet>
       <ErrorMessage/>
       <p style={{'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': 24}}>Page does not exist</p>
       <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': 24, 'marginTop': 30}} 
--- src/components/pages/SingleCharacterPage.js
+++ src/components/pages/SingleCharacterPage.js
@@ -1,11 +1,18 @@
+import { Helmet } from 'react-helmet';
 import './singleCharacterPage.scss';
-
   
 const SingleCharacterPage = ({data}) => {
     const {description, thumbnail, name} = data;
 
     return (
         <div className="single-character">
+          <Helmet>
+            <meta
+              name="description"
+              content={`${name} page`}
+            />
+            <title>{name}</title>
+          </Helmet>
             <img src={thumbnail} alt={name} className="single-character__img"/>
             <div className="single-character__info">
                 <h2 className="single-character__name">{name}</h2>
